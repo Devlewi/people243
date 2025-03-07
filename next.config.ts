844b77ac57055347f3243619d0c03f0022abdd10
devlewi <lewimarcjean3@gmail.com>
2025-03-07 00:37:22 +0000

    up

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {    
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'localhost',
+        pathname: '/wp-content/uploads/**', // Autorise les images depuis localhost
+      },
+      {
+        protocol: 'https',
+        hostname: 'controlpanel.people243.com', // Ajoute ce domaine
+        pathname: '/wp-content/uploads/**', // Autorise les images depuis ce chemin spécifique
+      },
+    ],
     domains: ['people243.com','controlpanel.people243.com', 'people243.vercel.app','client.cynomedia-africa.com'], // Ajoutez ici le domaine de votre image
   },
   
