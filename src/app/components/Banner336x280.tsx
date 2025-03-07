6be87bcb95430746f32013f87976a827a5f49bed
devlewi <lewimarcjean3@gmail.com>
2025-03-07 08:19:42 +0000

    desactivation de l optimisation des images 2

--- src/app/components/Banner336x280.tsx
+++ src/app/components/Banner336x280.tsx
@@ -1,12 +1,23 @@
 "use client";
 
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
 const Banner336x280: React.FC = () => {
   const [isClient, setIsClient] = useState(false);
+  const [randomNumber, setRandomNumber] = useState<number>(Math.floor(Math.random() * 1000000));
 
   useEffect(() => {
     setIsClient(true);
+  }, []);
+
+  useEffect(() => {
+    // Génère un nouveau numéro aléatoire toutes les 30 secondes, si nécessaire
+    const interval = setInterval(() => {
+      setRandomNumber(Math.floor(Math.random() * 1000000));
+    }, 30000);
+
+    return () => clearInterval(interval);
   }, []);
 
   if (!isClient) return null; // Empêche le rendu côté serveur
@@ -15,16 +26,24 @@ const Banner336x280: React.FC = () => {
     <div>
       {/* Affichage de l'iframe publicitaire */}
       <iframe
-        id="a5699f99"
-        name="a5699f99"
-        src={`https://client.cynomedia-africa.com/www/delivery/afr.php?zoneid=208&cb=${Date.now()}`}
+        id="a178c37c"
+        name="a178c37c"
+        src={`https://client.cynomedia-africa.com/www/delivery/afr.php?zoneid=218&cb=${randomNumber}`}
         frameBorder="0"
         scrolling="no"
         width="336"
         height="280"
         allow="autoplay"
-        style={{ display: "block", margin: "0 auto" }}
-      ></iframe>
+      >
+        <a href={`https://client.cynomedia-africa.com/www/delivery/ck.php?n=ab1357a3&cb=${randomNumber}`} target="_blank" rel="noopener noreferrer">
+          <Image
+            src={`https://client.cynomedia-africa.com/www/delivery/avw.php?zoneid=218&cb=${randomNumber}&n=ab1357a3`}
+            width={336}
+            height={280}
+            alt="Advertisement"
+          />
+        </a>
+      </iframe>
     </div>
   );
 };
