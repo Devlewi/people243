5cf18e1fc6acb6f54493971d2b3f11b374ab84df
devlewi <lewimarcjean3@gmail.com>
2026-02-04 15:14:22 +0000

    fix sitemap 1

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,12 +1,17 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.people243.com.com',  // Ton domaine principal
-    generateRobotsTxt: true,  // Génère un fichier robots.txt
-    sitemapSize: 5000,        // Nombre max d'URLs par sitemap (optionnel)
-    changefreq: 'daily',      // Fréquence d'update
-    priority: 0.7,            // Priorité des pages
+    siteUrl: 'https://www.people243.com',
+    generateRobotsTxt: true, 
+    sitemapSize: 5000,
+    changefreq: 'daily',
+    priority: 0.7,
     additionalSitemaps: [
-      'https://www.people243.com.com/api/sitemap-articles',  // URL de l'API qui génère le sitemap des articles
+      'https://www.people243.com/api/sitemap-articles',
     ],
-  };
-  
+    // AJOUTE CECI pour forcer l'apparition dans le robots.txt
+    robotsTxtOptions: {
+      additionalSitemaps: [
+        'https://www.people243.com/api/sitemap-articles',
+      ],
+    },
+};
